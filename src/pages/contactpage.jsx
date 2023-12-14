bbd22a843bc40272ed45ab41d7fc909c5f2662bd
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 00:54:34 +0700

    fix: animation in contact me

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -20,7 +20,7 @@ const ContactPage = () => {
     >
       <h3
         data-aos="fade-up"
-        data-aos-offset="-75"
+        data-aos-offset="75"
         data-aos-duration="1000"
         data-aos-easing="ease-in-out"
         data-aos-mirror="true"
@@ -97,7 +97,7 @@ const ContactPage = () => {
         </div>
         <div
           data-aos="fade-left"
-          data-aos-offset="-50"
+          data-aos-offset="50"
           data-aos-delay="100"
           data-aos-duration="1200"
           data-aos-easing="ease-in-out"

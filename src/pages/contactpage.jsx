2b108a909eed2e4c5b36d1df06684823a2dd37a2
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 12:30:04 +0700

    fix: animation contact me

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -30,16 +30,17 @@ const ContactPage = () => {
       >
         Contact Me
       </h3>
-      <div
-        data-aos="fade-right"
-        data-aos-offset="-50"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-mirror="true"
-        data-aos-once="true"
-        className="flex w-full flex-col items-center justify-center md:flex-row md:items-start"
-      >
-        <div className="p-2 md:mr-2 md:w-1/2">
+      <div className="flex w-full flex-col items-center justify-center md:flex-row md:items-start">
+        <div
+          data-aos="fade-right"
+          data-aos-offset="-50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="true"
+          data-aos-anchor-placement="top-center"
+          className="p-2 md:mr-2 md:w-1/2"
+        >
           <form
             onSubmit={(e) => e.preventDefault()}
             className="flex flex-col items-center gap-4"
@@ -95,87 +96,86 @@ const ContactPage = () => {
             </div>
           </form>
         </div>
-        <div className="md:ml-2 md:w-1/2">
-          <div
-            data-aos="fade-left"
-            data-aos-offset="75"
-            data-aos-duration="1000"
-            data-aos-easing="ease-in-out"
-            data-aos-mirror="true"
-            data-aos-once="true"
-          >
-            <h4 className="text-shadow text-center text-2xl font-semibold text-primary dark:text-dark-light  md:text-3xl lg:text-4xl">
-              Touch Me If You Can
-            </h4>
-            <ul className="inline-block">
-              <li className="my-2">
-                <a className="flex items-center gap-2">
-                  <MdOutlineEmail
-                    size={30}
-                    className="text-primary dark:text-dark-light"
-                  />
-                  <span>alifichiganjati@gmail.com</span>
-                </a>
-              </li>
-              <li className="my-2">
-                <a
-                  href="https://www.linkedin.com/in/alifi-chiganjati-5141bb24b/"
-                  target="_blank"
-                  className="flex items-center gap-2"
-                  rel="noreferrer"
-                >
-                  <FaLinkedin
-                    size={30}
-                    className="text-primary dark:text-dark-light"
-                  />
-                  <span>Alifi Chiganjati</span>
-                </a>
-              </li>
-              <li className="my-2">
-                <a
-                  href="https://github.com/AlifiChiganjati"
-                  target="_blank"
-                  className="flex items-center gap-2"
-                  rel="noreferrer"
-                >
-                  <FaGithub
-                    size={30}
-                    className="text-primary dark:text-dark-light"
-                  />
-                  <span>AlifiChiganjati</span>
-                </a>
-              </li>
+        <div
+          data-aos="fade-left"
+          data-aos-offset="75"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="true"
+          className="md:ml-2 md:w-1/2"
+        >
+          <h4 className="text-shadow text-center text-2xl font-semibold text-primary dark:text-dark-light  md:text-3xl lg:text-4xl">
+            Touch Me If You Can
+          </h4>
+          <ul className="inline-block">
+            <li className="my-2">
+              <a className="flex items-center gap-2">
+                <MdOutlineEmail
+                  size={30}
+                  className="text-primary dark:text-dark-light"
+                />
+                <span>alifichiganjati@gmail.com</span>
+              </a>
+            </li>
+            <li className="my-2">
+              <a
+                href="https://www.linkedin.com/in/alifi-chiganjati-5141bb24b/"
+                target="_blank"
+                className="flex items-center gap-2"
+                rel="noreferrer"
+              >
+                <FaLinkedin
+                  size={30}
+                  className="text-primary dark:text-dark-light"
+                />
+                <span>Alifi Chiganjati</span>
+              </a>
+            </li>
+            <li className="my-2">
+              <a
+                href="https://github.com/AlifiChiganjati"
+                target="_blank"
+                className="flex items-center gap-2"
+                rel="noreferrer"
+              >
+                <FaGithub
+                  size={30}
+                  className="text-primary dark:text-dark-light"
+                />
+                <span>AlifiChiganjati</span>
+              </a>
+            </li>
 
-              <li className="my-2">
-                <a
-                  href="https://www.instagram.com/alifi_chiganjati/"
-                  target="_blank"
-                  className="flex items-center gap-2"
-                  rel="noreferrer"
-                >
-                  <BsInstagram
-                    size={30}
-                    className="text-primary dark:text-dark-light"
-                  />
-                  <span>@alifi_chiganjati</span>
-                </a>
-              </li>
-              <li className="my-2">
-                <a
-                  href="https://twitter.com/AlifiChiganjati"
-                  target="_blank"
-                  className="flex items-center gap-2"
-                  rel="noreferrer"
-                >
-                  <FaSquareXTwitter
-                    size={30}
-                    className="text-primary dark:text-dark-light"
-                  />
-                  <span>@AlifiChiganjati</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+            <li className="my-2">
+              <a
+                href="https://www.instagram.com/alifi_chiganjati/"
+                target="_blank"
+                className="flex items-center gap-2"
+                rel="noreferrer"
+              >
+                <BsInstagram
+                  size={30}
+                  className="text-primary dark:text-dark-light"
+                />
+                <span>@alifi_chiganjati</span>
+              </a>
+            </li>
+            <li className="my-2">
+              <a
+                href="https://twitter.com/AlifiChiganjati"
+                target="_blank"
+                className="flex items-center gap-2"
+                rel="noreferrer"
+              >
+                <FaSquareXTwitter
+                  size={30}
+                  className="text-primary dark:text-dark-light"
+                />
+                <span>@AlifiChiganjati</span>
+              </a>
+            </li>
+          </ul>
           <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
             Don't be Shy, to get in touch with me if you have any projects or
             opportunities. I'm open to collaboration and eager to discuss how my

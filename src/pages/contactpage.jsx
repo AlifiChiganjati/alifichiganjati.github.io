2fd5836ea89fb778cb2297f09d999d68f8862b34
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 12:21:45 +0700

    fix: animation contact me

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -30,17 +30,16 @@ const ContactPage = () => {
       >
         Contact Me
       </h3>
-      <div className="flex w-full flex-col items-center justify-center md:flex-row md:items-start">
-        <div
-          data-aos="fade-right"
-          data-aos-offset="-50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="true"
-          data-aos-anchor-placement="top-center"
-          className="p-2 md:mr-2 md:w-1/2"
-        >
+      <div
+        data-aos="fade-right"
+        data-aos-offset="-50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="true"
+        className="flex w-full flex-col items-center justify-center md:flex-row md:items-start"
+      >
+        <div className="p-2 md:mr-2 md:w-1/2">
           <form
             onSubmit={(e) => e.preventDefault()}
             className="flex flex-col items-center gap-4"
@@ -108,7 +107,7 @@ const ContactPage = () => {
             <h4 className="text-shadow text-center text-2xl font-semibold text-primary dark:text-dark-light  md:text-3xl lg:text-4xl">
               Touch Me If You Can
             </h4>
-            <ul className="flex flex-col">
+            <ul className="inline-block">
               <li className="my-2">
                 <a className="flex items-center gap-2">
                   <MdOutlineEmail
@@ -177,15 +176,7 @@ const ContactPage = () => {
               </li>
             </ul>
           </div>
-          <p
-            data-aos="fade-up"
-            data-aos-offset="100"
-            data-aos-duration="1000"
-            data-aos-easing="ease-in-out"
-            data-aos-mirror="true"
-            data-aos-once="true"
-            className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl"
-          >
+          <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
             Don't be Shy, to get in touch with me if you have any projects or
             opportunities. I'm open to collaboration and eager to discuss how my
             skills and experience can contribute to your success. Whether it's a

3e0e6d13cf9a69f850bb41bd46af2d0552636324
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 11:42:09 +0700

    style: change style sidenav active

--- src/components/Elements/Nav/NavLinks.jsx
+++ src/components/Elements/Nav/NavLinks.jsx
@@ -9,14 +9,14 @@ const NavLinks = ({
   return (
     <li>
       <Link
-        activeClass={`relative text-text-primary ease-in-out duration-500 after:absolute after:-bottom-2 ${variant} after:left-0 after:w-full after:h-1 after:content-['']  after:rounded-xl dark:text-light`}
+        activeClass={`relative text-dark-light lg:text-text-primary ease-in-out duration-500 after:absolute after:-bottom-2 ${variant} after:left-0 after:w-full after:h-1 after:content-['']  after:rounded-xl dark:text-light`}
         to={to}
         spy={true}
         smooth={true}
         offset={-80}
         duration={500}
         onClick={onClick}
-        className="navLink  mx-4 cursor-pointer text-lg font-semibold text-light dark:text-dark-text-primary md:text-xl lg:text-2xl"
+        className="navLink  mx-4 cursor-pointer text-lg font-semibold text-text-primary dark:text-dark-text-primary md:text-xl lg:text-2xl"
       >
         {children}
       </Link>

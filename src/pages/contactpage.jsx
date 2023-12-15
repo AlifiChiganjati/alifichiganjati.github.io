3c5942c97afd7daa2522e9252f15be26f3934bf0
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 12:44:28 +0700

    fix: animation contact me

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -177,21 +177,15 @@ const ContactPage = () => {
               </li>
             </ul>
           </div>
-          <div
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-easing="ease-in-out"
-            data-aos-once="true"
-          >
-            <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
-              Don't be Shy, to get in touch with me if you have any projects or
-              opportunities. I'm open to collaboration and eager to discuss how
-              my skills and experience can contribute to your success. Whether
-              it's a one-time project or an ongoing partnership, I'm here to
-              explore and create value together. you can contact me via email or
-              social media, and I'll reply ASAP.
-            </p>
-          </div>
+
+          <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
+            Don't be Shy, to get in touch with me if you have any projects or
+            opportunities. I'm open to collaboration and eager to discuss how my
+            skills and experience can contribute to your success. Whether it's a
+            one-time project or an ongoing partnership, I'm here to explore and
+            create value together. you can contact me via email or social media,
+            and I'll reply ASAP.
+          </p>
         </div>
       </div>
       <Footer />

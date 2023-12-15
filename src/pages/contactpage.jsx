286aae43cff33020516d8e1e613d1732fe443a5e
alifichiganjati <alifichiganjati@gmail.com>
2023-12-15 12:48:05 +0700

    fix: animation contact me

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -108,8 +108,8 @@ const ContactPage = () => {
             <h4 className="text-shadow text-center text-2xl font-semibold text-primary dark:text-dark-light  md:text-3xl lg:text-4xl">
               Touch Me If You Can
             </h4>
-            <ul className="flex flex-col gap-2">
-              <li className="my-2">
+            <ul className="inline-block">
+              <li className="my-4">
                 <a className="flex items-center gap-2">
                   <MdOutlineEmail
                     size={30}
@@ -118,7 +118,7 @@ const ContactPage = () => {
                   <span>alifichiganjati@gmail.com</span>
                 </a>
               </li>
-              <li className="my-2">
+              <li className="my-4">
                 <a
                   href="https://www.linkedin.com/in/alifi-chiganjati-5141bb24b/"
                   target="_blank"
@@ -132,7 +132,7 @@ const ContactPage = () => {
                   <span>Alifi Chiganjati</span>
                 </a>
               </li>
-              <li className="my-2">
+              <li className="my-4">
                 <a
                   href="https://github.com/AlifiChiganjati"
                   target="_blank"
@@ -147,7 +147,7 @@ const ContactPage = () => {
                 </a>
               </li>
 
-              <li className="my-2">
+              <li className="my-4">
                 <a
                   href="https://www.instagram.com/alifi_chiganjati/"
                   target="_blank"
@@ -161,7 +161,7 @@ const ContactPage = () => {
                   <span>@alifi_chiganjati</span>
                 </a>
               </li>
-              <li className="my-2">
+              <li className="my-4">
                 <a
                   href="https://twitter.com/AlifiChiganjati"
                   target="_blank"
@@ -177,15 +177,23 @@ const ContactPage = () => {
               </li>
             </ul>
           </div>
-
-          <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
-            Don't be Shy, to get in touch with me if you have any projects or
-            opportunities. I'm open to collaboration and eager to discuss how my
-            skills and experience can contribute to your success. Whether it's a
-            one-time project or an ongoing partnership, I'm here to explore and
-            create value together. you can contact me via email or social media,
-            and I'll reply ASAP.
-          </p>
+          <div
+            data-aos="fade-left"
+            data-aos-offset="75"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="true"
+          >
+            <p className="mt-5  px-2 text-justify text-base font-light text-dark-primary dark:text-dark-light md:pr-4 md:text-xl">
+              Don't be Shy, to get in touch with me if you have any projects or
+              opportunities. I'm open to collaboration and eager to discuss how
+              my skills and experience can contribute to your success. Whether
+              it's a one-time project or an ongoing partnership, I'm here to
+              explore and create value together. you can contact me via email or
+              social media, and I'll reply ASAP.
+            </p>{" "}
+          </div>
         </div>
       </div>
       <Footer />

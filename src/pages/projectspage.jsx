45023a0857f4ca46db8d23149b113c4da4679704
Alifi Chiganjati@ <alifichiganjati@gmail.com>
2024-01-22 10:44:35 +0700

    feat: add project web quran

--- src/pages/projectspage.jsx
+++ src/pages/projectspage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import AlquranWeb from "../assets/Image/alquran-web.png";
 
 const ProjectPage = () => {
   useEffect(() => {
@@ -36,18 +37,26 @@ const ProjectPage = () => {
         data-aos-anchor-placement="top-center"
         className="mt-5 flex flex-wrap items-center justify-center gap-5 "
       >
-        <div className="flex h-auto w-[300px] cursor-pointer flex-col items-center justify-center gap-5 border border-primary opacity-75 shadow hover:opacity-100 active:opacity-100 dark:border-light">
-          <div className="">
-            <img src="https://placehold.co/300x300" alt="Image of project" />
+        <a
+          href="https://alifichiganjati.github.io/al-quran/"
+          target="_blank"
+          className="flex h-[490px] w-[300px] cursor-pointer flex-col items-center justify-start gap-5 border border-primary opacity-75 shadow hover:opacity-100 active:opacity-100 dark:border-light"
+          rel="noreferrer"
+        >
+          <div className="h-[298px] w-[298px] bg-cover">
+            <img
+              src={AlquranWeb}
+              alt="Image of project"
+              className="h-full w-full"
+            />
           </div>
           <h4 className="px-2  text-xl font-semibold dark:text-light md:text-2xl lg:text-3xl">
-            Title of Project
+            Al-Quran
           </h4>
           <p className="p-2 text-justify  dark:text-dark-text-primary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe enim
-            perferendis quis ad ut laudantium odio unde,
+            Website Al-Qur'an menggunakan API dari Kemenag
           </p>
-        </div>
+        </a>
         <div className="flex h-auto w-[300px] cursor-pointer flex-col items-center justify-center gap-5 border border-primary opacity-75 shadow hover:opacity-100 active:opacity-100 dark:border-light">
           <div className="">
             <img src="https://placehold.co/300x300" alt="Image of project" />

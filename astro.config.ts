14d9826e3d81efebe93cd7b84afcf02237f9f4a3
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-04-26 13:29:00 +0800

    FEAT: change workflow using bun and add bun.lock

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alifichiganjati.github.io",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
